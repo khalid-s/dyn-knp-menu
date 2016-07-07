c5e1f7f9f2bfe691c53471c436c5f9c0cc3c513e
steven <steven.illien@gmail.com>
2016-07-07 13:55:32 +0200

    Dynamic multi levels knp menu

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -14,16 +14,45 @@ class MenuBuilder implements ContainerAwareInterface
     public function main(FactoryInterface $factory, array $options)
     {
         $main = $factory->createItem('root');
-        
+
         // End result
-        // $main->addChild('Layout',array('route' => 'menu_index'));
-        // $main['Layout']->addChild('Node',array('route' => 'menu_index'));
+        // $what = $main->addChild('Layout',array('route' => 'menu_index'));
+        // // var_dump($what);
+        // $what->addChild('Node',array('route' => 'menu_index'));
         // $main['Layout']['Node']->addChild('Slider',array('route' => 'menu_index'));
 
         $em = $this->container->get('doctrine')->getManager();
 
-        $menus = $em->getRepository('AppBundle:Menu')->findAll();
+        $dataEntries = $em->getRepository('AppBundle:Menu')->findAll();
+
+        // Loop over each menu entry
+        foreach ($dataEntries as $dataEntry) {
+            // Handle only the parents in this loop
+            if (null !== $dataEntry->getParent()) continue;
+            $parentKnpEntry = $main->addChild($dataEntry->getName(), ['route' => 'menu_index']);
+
+            // Call a recursive function to bind childrens to their parent.
+            $this->bindChildrensToParent($dataEntry, $parentKnpEntry);
+        }
+
 
         return $main;
+    }
+
+    /**
+     * Recursively binds childs to their parent
+     *
+     * @param  $dataEntry  - An instance of entity Menu
+     * @param  $knpEntry   - A reference to a "knp entry" (returned by the "addChild" method)
+     */
+
+    private function bindChildrensToParent($dataEntry, $knpEntry) {
+        $childrens = $dataEntry->getChildren();
+        if (0 === count($childrens)) return;
+
+        foreach ($childrens as $child) {
+            $childKnpEntry = $knpEntry->addChild($child->getName(), ['route' => 'menu_index']);
+            $this->bindChildrensToParent($child, $childKnpEntry);
+        }
     }
 }
